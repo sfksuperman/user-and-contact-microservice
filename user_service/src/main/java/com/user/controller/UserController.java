badf53ac68d494c58519c08ec092ea6dcfa52179
Shah Faisal Khan <34414624+sfksuperman@users.noreply.github.com>
2022-03-14 13:52:16 +0530

    Update UserController.java

--- user_service/src/main/java/com/user/controller/UserController.java
+++ user_service/src/main/java/com/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.user.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import com.user.entity.Contact;
 import com.user.entity.User;
 import com.user.service.UserService;
 
@@ -15,8 +18,15 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping("/{userId}")
 	public User getUser(@PathVariable("userId") Long userId) {
-		return this.userService.getUser(userId);
+		
+		User user = this.userService.getUser(userId);
+		List<Contact> contacts = this.restTemplate.getForObject("http://contact-service/contact/user/" + userId, List.class);
+		user.setContacts(contacts);
+		return user;
 	}
 }
