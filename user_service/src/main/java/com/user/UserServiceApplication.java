87eeac48cd4e408285a5e659b3e9a529fe23e782
Shah Faisal Khan <34414624+sfksuperman@users.noreply.github.com>
2022-03-14 13:51:34 +0530

    Update UserServiceApplication.java

--- user_service/src/main/java/com/user/UserServiceApplication.java
+++ user_service/src/main/java/com/user/UserServiceApplication.java
@@ -2,6 +2,8 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -10,4 +12,9 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
